ddf251b4770b665bcf28824b0d3511c4f4947380
nikhil94058 <kumarnikhil94058@gmail.com>
2024-04-28 14:36:39 +0530

    Button Fixed JoinUs in Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,13 +14,15 @@ const Navbar = () => {
             <a href="/Almuni" className='p-2 font-["Coluna"] p-[0.125rem 0.5rem] m-[1.875rem] font-semibold rounded hover:rounded-full hover:border-[0.25rem] border-black hover:font-extrabold'>ALUMNI</a>
             <a href="/Gallery" className='p-2 font-["Coluna"] p-[0.125rem 0.5rem] m-[1.875rem] font-semibold rounded hover:rounded-full hover:border-[0.25rem] border-black hover:font-extrabold'>ART</a>
             <a href="/" className='p-2 font-["Coluna"] p-[0.125rem 0.5rem] m-[1.875rem] font-semibold rounded hover:rounded-full hover:border-[0.25rem] border-black hover:font-extrabold'>LITERARY</a>
+
+            <button
+              type="button" href="/"
+              className="text-black p-[0.125rem 0.5rem] p-2 h-[3.2rem] text-[1.25rem] w-[8.2rem] font-['Coluna'] font-extrabold mr-16 bg-[#F4B24E] hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm text-center 2 dark:focus:ring-yellow-900 hover:border-[0.25rem] border-black "
+            >
+              Join Us
+            </button>
           </div>
-          <button
-            type="button" href="/"
-            className="text-black text-[1.25rem] w-[12.5rem] pt-2 mt-2  font-['Coluna']  mr-16 bg-[#F4B24E] hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900 hover:border-[0.25rem] border-black" style={{ font: 'bold' }}
-          >
-            Join Us
-          </button>
+
 
         </ul>
       </div>
