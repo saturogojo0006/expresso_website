5e03a5f512381e440fb966be960f75289166d048
Lakshya <harshitlucky2004@gmail.com>
2024-06-04 21:47:25 +0530

    about page

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Image from 'next/image';
+// import bus from "../public/contactus.png";
 // import artblackbg from "../About Us Photos/art_img.png"
-// import bus from "../About Us Photos/32 1.png"
+import image from "../About Us Photos/Group 316.png"
 const About = () => {
   return (
     <>
@@ -8,15 +10,29 @@ const About = () => {
         <div className='flex'>      
           <div className='flex-row   w-1/2 pl-8'>
             <button className="border-4 border-black rounded-3xl mx-60 px-10 py-2 text-4xl bg-zinc-900 text-white hover:bg-zinc-700 hover:scale-110 font-serif">LITERATURE</button>
-           <img src="/" className="h-full w-full" alt="bg-black"/>
+           {/* <img src="" className="h-full w-full" alt=""/> */}
           </div>
           <div  className='flex-row   w-1/2  border-black pl-4'>
-          <button className="border-4 border-black rounded-3xl mx-60 px-24 py-2 text-4xl bg-gray-200 hover:bg-gray-300 hover:scale-110 font-serif">ART</button>
+          <button className="border-4 border-black rounded-3xl mx-80 px-24 py-2 text-4xl bg-gray-200 hover:bg-gray-300 hover:scale-110 font-serif">ART</button>
           </div>
         </div>
           <hr/>
+          <div className="flex justify-end mt-1">
+  {/* <div className="border-4 border-black relative"> */}
+    <Image
+      src={image}
+      alt="Bus Image"
+      width={1200} // Adjust the width as needed
+      height={800} // Adjust the height as needed
+      className="p-1 m-0"
+      style={{ marginTop: "-120px"}}
+      styleb={{ marginBottom: "-50px" }}
+    />
+  </div>
+{/* </div> */}
 
-        <div className="relative h-80  ">
+
+        <div className="relative h-96 mt-[-100px]  ">
           <div className="w-40 h-48 bg-orange-300 rounded-full ml-7"></div>
 
           <div className="absolute mb-20 top-8 bg-slate-900 px-12 pt-6 pb-3 mx-6 rounded-3xl my-3 text-white flex items-start gap-x-8 justify-between">
