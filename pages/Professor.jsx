f295e74c65c3b7a55f87be967aeaabb84e885809
Satyam1345 <Satyamkumar1345@gmail.com>
2024-06-05 13:22:07 +0530

    About Page Responsiveness Done, and file name changed from .js to .jsx

--- pages/Professor.jsx
+++ pages/Professor.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from 'next/image';
 import styles from "./Professor.module.css";
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import Image from "next/image";
 
 
 function Professor() {
