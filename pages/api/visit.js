57057c04b122789ec3beac7065435199016e160a
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2025-02-21 22:43:24 +0530

    added visitor count

--- pages/api/visit.js
+++ pages/api/visit.js
@@ -1,13 +1,35 @@
-let visitCounts = {};
+import mongoose from 'mongoose';
 
-export default function handler(req, res) {
-  const { website } = req.query;
+const MONGO_URI = process.env.MONGO_URI;
 
-  if (!visitCounts[website]) {
-    visitCounts[website] = 0;
+const visitorSchema = new mongoose.Schema({
+  name: String,
+  count: Number,
+});
+
+const Visitor = mongoose.models.Visitor || mongoose.model("Visitor", visitorSchema);
+
+mongoose.connect(MONGO_URI);
+
+export default async function handler(req, res) {
+  try {
+    let visitors = await Visitor.findOne({ name: "visits" });
+
+    if (!visitors) {
+      const beginCount = new Visitor({
+        name: "visits",
+        count: 1,
+      });
+
+      await beginCount.save();
+      res.status(200).json({ count: 1 });
+    } else {
+      visitors.count += 1;
+      await visitors.save();
+      res.status(200).json({ count: visitors.count });
+    }
+  } catch (err) {
+    console.error("Error fetching visitor count:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
-  visitCounts[website] += 1;
-
-  res.status(200).json({ website, visits: visitCounts[website] });
 }
